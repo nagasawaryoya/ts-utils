7c5633925b4c8662a7e3aaed89e66fab06b63b83
nagasawaryoya <nagasawaryoya@gmail.com>
2021-02-06 22:55:56 +0900

    文字列フォーマットメソッド追加

--- src/utils/stringUtil.ts
+++ src/utils/stringUtil.ts
@@ -1,4 +1,19 @@
 /**
  * 文字列ユーティリティークラス
  */
-export class stringUtil {}
+export class stringUtil {
+  /**
+   * メッセージ文フォーマット
+   * 'あああ{0}いいいい{1}うううう'などの可変指定メッセージのフォーマットを行う。
+   *
+   * @param string text メッセージ文
+   * @param any[] params 可変値パラメータ
+   * @returns フォーマット後の文字列
+   */
+  public static format(text: string, ...params: any[]): string {
+    for (let i = 0; i < params.length; i += 1) {
+      text = text.replace(new RegExp('\\{' + i + '\\}', 'g'), params[i]);
+    }
+    return text;
+  }
+}
