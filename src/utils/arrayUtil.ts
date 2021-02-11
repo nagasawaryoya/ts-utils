97f087346b4f54fef92403bfab4000d931413c5c
nagasawaryoya <nagasawaryoya@gmail.com>
2021-02-11 18:18:49 +0900

    ArrayUtilにswapメソッド追加

--- src/utils/arrayUtil.ts
+++ src/utils/arrayUtil.ts
@@ -309,4 +309,27 @@ export class ArrayUtil {
 
     return Array.from<T>(difference);
   }
+
+  /**
+   * 引数で受け取った要素同士の配列内の順序を入れ替える。
+   *
+   * @param {T[]} array 順序入れ替え前の配列
+   * @param {number} index1 入れ替える要素のindex1
+   * @param {number} index2 入れ替える要素のindex2
+   * @returns {T[]} 順序入れ替え後の配列
+   */
+  public static swap<T>({
+    array,
+    index1,
+    index2,
+  }: {
+    array: T[];
+    index1: number;
+    index2: number;
+  }): T[] {
+    const tmp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = tmp;
+    return array;
+  }
 }
