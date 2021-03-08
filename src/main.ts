d6c5358d75ca0548eddc0424b18f55506abed25c
nagasawaryoya <nagasawaryoya@gmail.com>
2021-03-08 21:05:21 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,167 +1,131 @@
-// /**
-//  * 引数で受け取った配列を挿入ソートして返却する。
-//  *
-//  * @param arr number[] ソートする配列
-//  * @returns number[]
-//  */
-// function insert(arr: number[], count: number = 0): number[] {
-//   let current = count;
-//   while (count < arr.length) {
-//     if (current >= 0 && arr[current] < arr[current - 1]) {
-//       arr = swap({ array: arr, index1: current, index2: current - 1 });
-//       current--;
-//     } else {
-//       current = count++;
-//     }
-//   }
-//   return arr;
-// }
-
-// /**
-//  * 引数で受け取った配列を挿入ソートして返却する。(再帰ver)
-//  *
-//  * @param arr number[] ソートする配列
-//  * @returns number[]
-//  */
-// function insertRecursion(arr: number[], count: number = 0): number[] {
-//   let current = count;
-//   while (current >= 0 && arr[current] < arr[current - 1]) {
-//     arr = swap({ array: arr, index1: current, index2: current - 1 });
-//     current--;
-//   }
-//   return count === arr.length ? arr : insertRecursion(arr, count + 1);
-// }
-
-// /**
-//  * 引数で受け取った要素同士の配列内の順序を入れ替える。
-//  *
-//  * @param arr number[] ソートする配列
-//  * @returns number[]
-//  */
-// function swap({
-//   array,
-//   index1,
-//   index2,
-// }: {
-//   array: number[];
-//   index1: number;
-//   index2: number;
-// }): number[] {
-//   const tmp = array[index1];
-//   array[index1] = array[index2];
-//   array[index2] = tmp;
-//   return array;
-// }
-
-// /***
-//  * min以上max未満の乱数を返却する。
-//  *
-//  * @param number min 最小値（整数）
-//  * @param number max 最大値（整数）
-//  * @returns number 乱数
-//  */
-// function getRandomInt({ min, max }: { min: number; max: number }): number {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min)) + min;
-// }
-// /**
-//  * 整数の乱数配列を生成する。
-//  *
-//  * @param min number 最小値（整数）
-//  * @param length number 生成する配列の長さ
-//  * @returns 整数の乱数配列
-//  */
-// function getRandomArray({
-//   min,
-//   length,
-// }: {
-//   min: number;
-//   length: number;
-// }): number[] {
-//   const initAry: number[] = [];
-//   const arr: number[] = [];
-//   for (let i = 0; i < length; i++) {
-//     initAry.push(min + i);
-//   }
-//   while (initAry.length > 0) {
-//     arr.push(
-//       initAry
-//         .splice(getRandomInt({ min: min, max: initAry.length }), 1)
-//         .slice(-1)[0]
-//     );
-//   }
-//   return arr;
-// }
-// function start() {
-//   // const arr = getRandomArray({ min: 0, length: 8909 });
-//   const arr = getRandomArray({ min: 0, length: 10 });
-//   // 実行
-//   console.log(insert(arr));
-//   // console.log(isort(arr));
-// }
-// start();
-
-// ==================================================
-// ==================================================
-// ==================================================
-
-const test2: any[] = [
+var test = [
   {
-    id: '1',
-    child: [{ id: '2' }, { id: '3' }],
+      p: 1,
+      c: 1,
+      depath: 0
   },
   {
-    id: '4',
-    child: [{ id: '5' }, { id: '6' }],
+      p: 1,
+      c: 2,
+      depath: 1
   },
   {
-    id: '7',
-    child: [
-      { id: '8' },
-      {
-        id: '9',
-        child: [
-          { id: '10' },
-          {
-            id: '11',
-            child: [{ id: '12' }],
-          },
-        ],
-      },
-    ],
+      p: 1,
+      c: 3,
+      depath: 2
   },
+  // {
+  //     p: 1,
+  //     c: 4,
+  //     depath: 1
+  // },
+  // {
+  //     p: 1,
+  //     c: 9,
+  //     depath: 3
+  // },
+  // {
+  //     p: 1,
+  //     c: 10,
+  //     depath: 3
+  // },
+  // {
+  //     p: 1,
+  //     c: 11,
+  //     depath: 4
+  // },
+  {
+      p: 2,
+      c: 2,
+      depath: 0
+  },
+  {
+      p: 2,
+      c: 3,
+      depath: 1
+  },
+  // {
+  //     p: 3,
+  //     c: 3,
+  //     depath: 0
+  // },
+  // {
+  //     p: 4,
+  //     c: 4,
+  //     depath: 0
+  // },
+  // {
+  //     p: 5,
+  //     c: 5,
+  //     depath: 0
+  // },
+  // {
+  //     p: 6,
+  //     c: 6,
+  //     depath: 0
+  // },
+  // {
+  //     p: 7,
+  //     c: 7,
+  //     depath: 0
+  // },
+  // {
+  //     p: 8,
+  //     c: 8,
+  //     depath: 0
+  // },
 ];
 
-const removeTreeValue = ({
-  tree,
-  value,
-  targetKey = 'id',
-  childrenKey = 'children',
-}: {
-  tree: any[];
-  value: string | number;
-  targetKey?: string | number;
-  childrenKey?: string | number;
-}): any[] => {
-  return tree.filter((tr) => {
-    if (tr[targetKey] === value) {
-      return false;
-    } else if (tr[childrenKey] && tr[childrenKey].length > 0) {
-      tr[childrenKey] = removeTreeValue({
-        tree: tr[childrenKey],
-        value: value,
-        targetKey: targetKey,
-        childrenKey: childrenKey,
-      });
-    }
-    return true;
-  });
+const align = (roots: any[], nodes: any[], results: any[] = [], isNext = false): any => {
+  const currentRoot = getRootOne(roots);
+  // const participants = findParticipants(currentRoot[currentRoot.length - 1], nodes);
+  let participants = [];
+  console.log(currentRoot);
+  if (!isNext) {
+      participants = findParticipants(currentRoot.slice(-2)[0], nodes);
+  } else {
+      participants = findParticipants(currentRoot.slice(-1)[0], nodes);
+  }
+
+  if (participants.length) {
+      // 下階層がないか探索
+      isNext = false;
+      const underLevelIndex = findUnderLevelIndex(participants);
+      const node = nodes.splice(underLevelIndex, 1)[0];
+      currentRoot.push(node);
+  } else {
+      // 次にすすむ
+      isNext = true;
+      results.push(roots.shift());
+  }
+
+  return (!nodes.length || !roots.length) ? results : align(roots, nodes, results, isNext);
 };
 
-console.log(
-  removeTreeValue({
-    tree: test2,
-    value: '2',
-  })
+// ルートでないことを確認する
+const notRoot = (node: any) => node.p !== node.c;
+// ルートの関係者のノードの配列を返却
+const findParticipants = (root: any, nodes: any[]) => (
+  nodes
+  .filter((n: any) => notRoot(n))
+  .filter((n: any) => root.p === n.p)
 );
+// ルートのみの配列を作成する
+const findRoot = (roots: any) => roots.filter((root: any) => root.p === root.c).map((root: any) => [root]);
+// ルートの先頭を非破壊的に取得
+const getRootOne = (roots: any[]) => [...roots][0];
+// 直下の階層のノードを探索する
+const findUnderLevelIndex = (nodes: any) => {
+  const depaths = nodes
+      .filter((node: any) => notRoot(node))
+      .map((node: any) => node.depath);
+  return Math.min(...depaths);
+};
+
+const create = (array: any) => {
+  const roots = findRoot(array);
+  return align(roots, test);
+};
+
+// console.log(create(test));
+create(test);
